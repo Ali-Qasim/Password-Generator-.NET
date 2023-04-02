dee4de76a0f0bda37e790ef97bd5a4b9572619d6
Ali Q <67727388+Ali-Qasim@users.noreply.github.com>
2023-04-03 01:47:26 +0530

    added c#

---
+++ Backend.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace PasswordGenerator
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PasswordController : ControllerBase
+    {
+        private readonly Password _passwordGenerator;
+
+        public PasswordController()
+        {
+            _passwordGenerator = new Password(includeLowercase: true, includeUppercase: true, includeNumbers: true, includeSymbols: true);
+        }
+
+        [HttpGet("{length}")]
+        public ActionResult<string> GetPassword(int length)
+        {
+            var generatedPassword = _passwordGenerator.Generate(length);
+            return generatedPassword;
+        }
+    }
+
+    public class Password
+    {
+        private readonly List<string> _characterGroups = new List<string>();
+
+        public Password(bool includeLowercase, bool includeUppercase, bool includeNumbers, bool includeSymbols)
+        {
+            if (includeLowercase)
+            {
+                _characterGroups.Add("abcdefghijklmnopqrstuvwxyz");
+            }
+
+            if (includeUppercase)
+            {
+                _characterGroups.Add("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            }
+
+            if (includeNumbers)
+            {
+                _characterGroups.Add("0123456789");
+            }
+
+            if (includeSymbols)
+            {
+                _characterGroups.Add("!@#$%^&*()_+-={}[]\\|:;<>,.?/~`");
+            }
+        }
+
+        public string Generate(int length)
+        {
+            var rng = new Random();
+            var passwordChars = new List<char>();
+
+            for (int i = 0; i < length; i++)
+            {
+                var randomGroup = _characterGroups[rng.Next(_characterGroups.Count)];
+                passwordChars.Add(randomGroup[rng.Next(randomGroup.Length)]);
+            }
+
+            return new string(passwordChars.ToArray());
+        }
+    }
+}
---
+++ Frontend.cs
@@ -0,0 +1,25 @@
+﻿using PasswordGenerator;
+
+Console.WriteLine("Welcome to the Password Generator!");
+
+Console.Write("Enter desired password length: ");
+var passwordLength = int.Parse(Console.ReadLine());
+
+Console.Write("Include lowercase letters? (y/n): ");
+var includeLowercase = Console.ReadLine().ToLower() == "y";
+
+Console.Write("Include uppercase letters? (y/n): ");
+var includeUppercase = Console.ReadLine().ToLower() == "y";
+
+Console.Write("Include numbers? (y/n): ");
+var includeNumbers = Console.ReadLine().ToLower() == "y";
+
+Console.Write("Include symbols? (y/n): ");
+var includeSymbols = Console.ReadLine().ToLower() == "y";
+
+var password = new Password(includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+var generatedPassword = password.Generate(passwordLength);
+
+Console.WriteLine($"Your generated password is: {generatedPassword}");
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
